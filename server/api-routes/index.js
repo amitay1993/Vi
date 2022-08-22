b68b30448f6ad3fd6026b199c09c8ccd6e8ad3fb
Amitay Tsinis <43268645+amitay1993@users.noreply.github.com>
2022-08-22 12:46:03 +0300

    separate routes

---
+++ server/api-routes/index.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { API_ROUTES } from "./apiRoutes.consts.js";
+import { moviesPerActorApi } from "./moviesPerActor/index.js";
+import { charactersWithMultipleActorsApi } from "./charactersWithMultipleActors/index.js";
+import { actorsWithMultipleCharactersApi } from "./actorsWiwthMultipleCharcters/index.js";
+
+export function apiRoutes() {
+  const router = Router();
+  router.use(API_ROUTES.moviesPerActor, moviesPerActorApi());
+  router.use(
+    API_ROUTES.actorsWithMultipleCharacters,
+    actorsWithMultipleCharactersApi()
+  );
+  router.use(
+    API_ROUTES.charactersWithMultipleActors,
+    charactersWithMultipleActorsApi()
+  );
+  return router;
+}
